3026a0c5f982cf0747326da95178c33579d29a12
mizmorr <mizmorrr@gmail.com>
2024-04-09 13:02:14 +0300

    Revert "init 2"
    This reverts commit a540c41ae6fa214641dc1db7bcab748cc1922ba7.
    revert

--- new_image/static/script.js
+++
@@ -1,9 +0,0 @@
-range=document.getElementById('myRange');
-val=document.getElementById('value');
-val.value="Value: "+range.value;
-
-
-  range.addEventListener('change', function(){
-    val.value="Value: "+range.value;
-    
-  })
