8cddb39fc4e9ff7da1066f7833a1c696891e43e3
mizmorr <safonov.ilya22@gmail.com>
2024-05-23 02:01:18 +0300

    get slaves request handler

--- referencer/image/main.go
+++ referencer/image/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -12,13 +14,34 @@ func logRequest(req *http.Request) {
 	uri := req.URL.String()
 	method := req.Method
 	header := req.Header
-	fmt.Println(uri, "\n", method, "\n", header, "\n---")
+	log.Println(uri, "\n", method, "\n", header, "\n---")
 }
 
 type Req_body struct {
 	Name string `json:"name"`
 }
 
+func get_slaves(name string) (res []string, err error) {
+	json_file, err := os.Open("slaves.json")
+	if err != nil {
+		return nil, err
+	}
+	var k map[string]interface{}
+	defer json_file.Close()
+	byte_value, _ := io.ReadAll(json_file)
+	json.Unmarshal(byte_value, &k)
+	result := k[name]
+	if result != nil {
+		m_ap := result.(map[string]interface{})
+		for k := range m_ap {
+			res = append(res, k)
+		}
+		return
+	} else {
+		return nil, fmt.Errorf("no slaves found")
+	}
+
+}
 func make_map() map[string]string {
 	res := make(map[string]string)
 	k, err := os.ReadFile("markup.txt")
@@ -31,6 +54,26 @@ func make_map() map[string]string {
 		res[curr[0]] = curr[1]
 	}
 	return res
+}
+func slaves_req_handler(r *http.Request, res http.ResponseWriter, ref map[string]string) {
+	decoder := json.NewDecoder(r.Body)
+	var req Req_body
+	err := decoder.Decode(&req)
+	if err != nil {
+		log.Fatal(err.Error())
+		panic(err)
+	}
+	name := req.Name
+	cluster_num := ref[name]
+	slaves, err := get_slaves(cluster_num)
+	if err != nil {
+		log.Fatal(err.Error())
+		fmt.Fprint(res, "")
+	} else {
+		logRequest(r)
+		fmt.Fprint(res, slaves)
+	}
+
 }
 func main() {
 	reference_map := make_map()
@@ -47,11 +90,14 @@ func main() {
 		logRequest(r)
 	})
 
+	http.HandleFunc("/get_slaves", func(w http.ResponseWriter, r *http.Request) {
+		slaves_req_handler(r, w, reference_map)
+	})
+
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "metrics is good")
 		logRequest(r)
 	})
-
 	fmt.Println("Server is listening port...")
 	http.ListenAndServe(":9080", nil)
 }
